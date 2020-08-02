46981d87f5968174ea74337bba045abbfa59678a
Kwanusu <user@Users-MacBook-Pro.local>
2020-08-03 01:50:18 +0300

    lesson 02

--- app/src/main/java/com/example/coderswag/controller/MainActivity.kt
+++ app/src/main/java/com/example/coderswag/controller/MainActivity.kt
@@ -2,12 +2,23 @@ package com.example.coderswag.controller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import com.example.coderswag.R
+import com.example.coderswag.model.Category
+import com.example.coderswag.services.dataservice
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var adapter : ArrayAdapter<Category>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        adapter = ArrayAdapter(this,
+            android.R.layout.simple_list_item_1,
+            dataservice.categories)
+        categoryListView.adapter = adapter
     }
 }
--- app/src/main/java/com/example/coderswag/model/category.kt
+++ app/src/main/java/com/example/coderswag/model/category.kt
@@ -2,4 +2,8 @@ package com.example.coderswag.model
 
 import android.media.Image
 
-class Category(val title: String, val image: String)
+class Category(val title: String, val image: String){
+    override fun toString(): String {
+        return title
+    }
+}
