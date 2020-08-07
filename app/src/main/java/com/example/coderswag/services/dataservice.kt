d63707b7ce8487d84fa970efcbaa53508a12c693
kwanusu <kipropkevin93@gmail.com>
2020-08-07 03:33:04 +0300

    lesson07

--- app/src/main/java/com/example/coderswag/services/dataservice.kt
+++ app/src/main/java/com/example/coderswag/services/dataservice.kt
@@ -20,21 +20,36 @@ object dataservice {
         Category("DIGITAL", "digitalgoodsimage")
     )
     val hats = listOf(
-        Product("Kwanusu headshot", "Kshs. 1500", "hat01"),
-        Product("Kwanusu king", "Kshs. 1300", "hat02"),
-        Product("Kwanusu liverpool", "Kshs. 1500", "hat03"),
-        Product("Kwanusu kenya", "Kshs. 1600", "hat04")
+        Product("Kwanusu headshot", "Kshs. 1500", "hat1"),
+        Product("Kwanusu king", "Kshs. 1300", "hat2"),
+        Product("Kwanusu liverpool", "Kshs. 1500", "hat3"),
+        Product("Kwanusu kenya", "Kshs. 1600", "hat4")
     )
     val hoodies = listOf(
-        Product("Kwanusu liverpool", "Kshs. 2500", "hoodie01"),
-        Product("Kwanusu arsenal", "Kshs. 2500", "hoodie02"),
-        Product("Kwanusu chelsea", "Kshs. 2500", "hoodie03"),
-        Product("Kwanusu everton", "Kshs. 2500", "hoodie04")
+        Product("Kwanusu liverpool", "Kshs. 2500", "hoodie1"),
+        Product("Kwanusu arsenal", "Kshs. 2500", "hoodie2"),
+        Product("Kwanusu chelsea", "Kshs. 2500", "hoodie3"),
+        Product("Kwanusu everton", "Kshs. 2500", "hoodie4")
     )
     val shirts = listOf(
-        Product("kwanusu LFChome", "Kshs. 1500", "shirt01"),
-        Product("kwanusu LFCaway", "Kshs. 1500", "shirt02"),
-        Product("kwanusu LFC3rdkit", "Kshs. 1500", "shirt03"),
-        Product("kwanusu LFCbaby", "Kshs. 1500", "shirt04")
+        Product("kwanusu LFChome", "Kshs. 1500", "shirt1"),
+        Product("kwanusu LFCaway", "Kshs. 1500", "shirt2"),
+        Product("kwanusu LFC3rdkit", "Kshs. 1500", "shirt3"),
+        Product("kwanusu LFCbaby", "Kshs. 1500", "shirt4")
     )
+
+    val digitalgoods = listOf<Product>()
+
+    fun getProducts(category: String) : List<Product> {
+        return when(category) {
+            "SHIRTS" -> shirts
+            "HOODIES" -> hoodies
+            "HATS" -> hats
+            else -> digitalgoods
+        }
+    }
+
+
+
+
 }
